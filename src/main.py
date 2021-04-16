e5e3a19a3f622a8505483b4cecbf620ac3209dcb
sorooshm78@gmail.com <sorooshm78@gmail.com>
2021-04-16 17:12:29 +0430

    add alogo

--- src/main.py
+++ src/main.py
@@ -8,6 +8,14 @@ def read_file(file_name, field):
 	file.close()
 	return result
 
+
+
+
+
+
+
+
+'''
 driver = webdriver.Firefox()
 driver.get(read_file("info.txt", "url"))
 
@@ -38,9 +46,106 @@ search.send_keys(stock)
 time.sleep(2)
 driver.find_elements_by_xpath("//div[contains(@isin, 'IR')]")[0].click()
 
+
+
+def convert_str_to_int(str):
+	return int(re.sub(',','', str))
+
+
 #price
 time.sleep(2)
-hight_price = int(re.sub(',','', driver.find_element_by_id("dailyslider_Hight").text))
-low_price = int(re.sub(',','', driver.find_element_by_id("dailyslider_Low").text))
+hight_price = convert_str_to_int(driver.find_element_by_id("dailyslider_Hight").text)
+low_price = convert_str_to_int(driver.find_element_by_id("dailyslider_Low").text)
+
 print("hight price:" + str(hight_price))
 print("low price:" + str(low_price))
+
+#Alogorithm
+
+min = int(read_file('info.txt', 'min'))
+
+#for i in range(1, min+1):
+
+
+now_sell_price = convert_str_to_int(driver.find_element_by_xpath('//table/tbody/tr[1]/td[3]/span[2]').text)
+now_buy_price = convert_str_to_int(driver.find_element_by_xpath('//table/tbody/tr[1]/td[4]/span[2]').text)
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+wage = 0.0025
+
+def write_file(file_name , count, price, last_buy_price, last_sell_price, money):
+	with open(file_name, 'w') as file:
+		file.write('count = %s\n' % count)
+		file.write('buy_price = %s\n' % price)
+		file.write('last_buy_price = %s\n' % last_buy_price)
+		file.write('last_sell_price = %s\n' % last_sell_price)
+		file.write('money = %s\n' % money)
+
+with open('list', 'r') as f:
+	for price in f.readlines():
+		print(int(price))
+
+		now_sell_price = int(price)
+		now_buy_price = int(price)
+
+		count = int(read_file('stock.txt', 'count'))
+		last_sell_price = int(read_file('stock.txt', 'last_sell_price'))
+		last_buy_price = int(read_file('stock.txt', 'last_buy_price'))
+		money = int(read_file('stock.txt', 'money'))
+
+		#SELL
+		if count != 0:
+			if now_sell_price < last_sell_price:
+
+				#update file
+				n_count = 0
+				n_buy_price = 0
+				n_last_buy_price = now_buy_price
+				n_last_sell_price = now_sell_price
+				n_money = money + int((now_sell_price * count) - (now_sell_price * count * wage))
+				write_file('stock.txt', n_count, n_buy_price, n_last_buy_price, n_last_sell_price, n_money)
+
+				print("sell count '{0}' , price '{1}'".format(count, now_sell_price))
+				continue
+				#sell()
+
+
+		#BUY
+		if count == 0:
+			if now_buy_price > last_buy_price:
+				
+				#update file
+				n_count = int(money/now_buy_price)
+				n_buy_price = now_buy_price
+				n_last_buy_price = now_buy_price
+				n_last_sell_price = now_sell_price
+				n_money = money - int((now_buy_price * n_count) + (now_buy_price * n_count * wage))
+				write_file('stock.txt', n_count, n_buy_price, n_last_buy_price, n_last_sell_price, n_money)
+
+				print("buy count '{0}' , price '{1}'".format(n_count, now_buy_price))
+				continue
+				#buy()				
+
+		n_count = count
+		n_buy_price = read_file('stock.txt', 'buy_price')
+		n_last_buy_price = now_buy_price
+		n_last_sell_price = now_sell_price
+		n_money = money
+		write_file('stock.txt', n_count, n_buy_price, n_last_buy_price, n_last_sell_price, n_money)
