3ace6b243d3c42fef2e40a13628075c5edf9fa99
sorooshm78@gmail.com <sorooshm78@gmail.com>
2021-04-16 18:46:37 +0430

    add algo

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
 import re
 import time
 from selenium import webdriver
+import datetime
+
+def convert_str_to_int(str):
+	return int(re.sub(',','', str))
 
 def write_file(file_name , count, price, last_buy_price, last_sell_price, money):
 	with open(file_name, 'w') as file:
@@ -33,27 +37,21 @@ login_butten = driver.find_element_by_class_name("btn-enter")
 login_butten.click()
 
 #abort popup box 
-time.sleep(2)
+time.sleep(5)
 if(len(driver.find_elements_by_class_name("popup-box")) != 0):
 	abort = driver.find_elements_by_class_name("close")[1]
 	abort.click()
 
 #search
-time.sleep(2)
+time.sleep(5)
 stock = read_file("info.txt", "stock")
 search = driver.find_elements_by_id("txt_search")[1]
 search.send_keys(stock)
 time.sleep(2)
 driver.find_elements_by_xpath("//div[contains(@isin, 'IR')]")[0].click()
 
-
-
-def convert_str_to_int(str):
-	return int(re.sub(',','', str))
-
-
 #price
-time.sleep(2)
+time.sleep(5)
 hight_price = convert_str_to_int(driver.find_element_by_id("dailyslider_Hight").text)
 low_price = convert_str_to_int(driver.find_element_by_id("dailyslider_Low").text)
 
@@ -69,10 +67,7 @@ for i in range(1, min+1):
 	now_sell_price = convert_str_to_int(driver.find_element_by_xpath('//table/tbody/tr[1]/td[3]/span[2]').text)
 	now_buy_price = convert_str_to_int(driver.find_element_by_xpath('//table/tbody/tr[1]/td[4]/span[2]').text)
 
-	log.write('sell price {0}, buy price {1}'.format(now_sell_price, now_buy_price))
-
-	now_sell_price = int(price)
-	now_buy_price = int(price)
+	log.write('Date:{0}		sell price {1}, buy price {2}\n'.format(datetime.datetime.now(), now_sell_price, now_buy_price))
 
 	count = int(read_file('stock.txt', 'count'))
 	last_sell_price = int(read_file('stock.txt', 'last_sell_price'))
@@ -93,7 +88,7 @@ for i in range(1, min+1):
 				write_file('stock.txt', n_count, n_buy_price, n_last_buy_price, n_last_sell_price, n_money)
 
 				print("sell count '{0}' , price '{1}'".format(count, now_sell_price))
-				log.write("sell count '{0}' , price '{1}', money '{2}'\n".format(count, now_sell_price, n_money))
+				log.write("Date:{0}		sell count {1} , price {2}, money {3}\n".format(datetime.datetime.now(), count, now_sell_price, n_money))
 				continue
 				#sell()
 
@@ -110,7 +105,7 @@ for i in range(1, min+1):
 			write_file('stock.txt', n_count, n_buy_price, n_last_buy_price, n_last_sell_price, n_money)
 
 			print("buy count '{0}' , price '{1}'".format(n_count, now_buy_price))
-			log.write("buy count '{0}' , price '{1}', money '{2}'\n".format(count, now_sell_price, n_money))
+			log.write("Date:{0}		buy count {1} , price {2}, money {3}\n".format(datetime.datetime.now(),n_count, now_sell_price, n_money))
 			continue
 			#buy()				
 
